c68bb61d5731cf78a5d3e2bbc3f3e8b3e566f64a
mkneumann <mkneumann777@icloud.com>
2023-09-22 19:47:07 -0700

    Updates

--- index.32dc8faf.js
+++
@@ -1,2 +0,0 @@
-!function(){if(document.getElementById("hero")){const e=document.createElement("button");e.classList.add("px-4","py-2","mr-4","rounded","border","border-white","bg-black","hover:bg-white","hover:text-black","transition-all","w-32"),e.innerText="Learn More",e.addEventListener("click",(()=>{var e;e="about",document.getElementById(e).scrollIntoView({behavior:"smooth"})})),document.getElementById("hero-cta").prepend(e)}}();
-//# sourceMappingURL=index.32dc8faf.js.map
